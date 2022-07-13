01ea58731202b4c678d036d82cd8e6ee58740cea
Carter Brown <carter.b@earthlink.net>
2022-07-13 02:45:57 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -12,6 +12,21 @@ namespace cse210_01
         {
             Console.WriteLine("Player 1: X and Player 2: O");
 
+            Board();
+        }
+    
+
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
         }
     }
 }
