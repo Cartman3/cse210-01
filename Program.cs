62cf59b8d7ae6e3c293848f99027890302f72b72
Carter Brown <carter.b@earthlink.net>
2022-07-13 03:36:08 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -5,9 +5,12 @@ namespace cse210_01
 {
     class Program
     {
+        // Array
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static int player = 1;
         static int choice;
+        // Flag helps check who won. If the value is 1 then someone won
+        static int flag = 0;
         static void Main(string[] args)
         {
             do
@@ -26,13 +29,13 @@ namespace cse210_01
                 }
                 else
                 {
-                    Console.WriteLine("Y's turn to choose a square (1-9): ");
+                    Console.WriteLine("O's turn to choose a square (1-9): ");
                 }
                 Console.WriteLine("\n");
                 // Calls the board
                 Board();
                 choice = int.Parse(Console.ReadLine());
-                // checks to see if the space has already been played or not
+                // Checks to see if the space has already been played or not
                 if (arr[choice] != 'X' && arr[choice] != 'O')
                 {
                     if (player % 2 == 0)
@@ -55,20 +58,21 @@ namespace cse210_01
                     Console.WriteLine("Please wait 2 second board is loading again.....");
                     Thread.Sleep(2000);
                 }
-                // Calls the CheckWinn fuction
+                // Call for CheckWin function
                 flag = CheckWin();
             }
+
+            // Will loop until all cell are filled or someone won
             while (flag != 1 && flag != -1);
-            Console.clear();
+            Console.Clear();
             Board();
-            
+
             if (flag == 1)
-            // if flag value is 1 then someone has win or
-            //means who played marked last time which has win
             {
                 Console.WriteLine("Player {0} has won", (player % 2) + 1);
             }
-            else// if flag value is -1 the match will be draw and no one is winner
+            // If flag value is -1 the match will be draw and no one is winner
+            else
             {
                 Console.WriteLine("Draw");
             }
@@ -88,9 +92,60 @@ namespace cse210_01
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
             Console.WriteLine("     |     |      ");
         }
+        // Checks to see if someone has won or not
         private static int CheckWin()
         {
-
+        
+            //Winning Condition For First Row
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Row
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Row
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            
+            //Winning Condition For First Column
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Column
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Column
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            // If all the cells or values filled with X or O then its a tie
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            
+            else
+            {
+                return 0;
+            }
         }
     }
 }
