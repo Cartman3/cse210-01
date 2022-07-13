647c381f039b40ad0c222c7d6a5a367bc1c4c96a
Carter Brown <carter.b@earthlink.net>
2022-07-13 00:16:49 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading;
 
 namespace cse210_01
 {
     class Program
     {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Player 1: X and Player 2: O");
+
+            Console.WriteLine("Testing my repository");
         }
     }
 }
+
